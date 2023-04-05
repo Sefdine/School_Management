d9331949d239be83bc47482b3ac0ac5b2aab99f2
Sefdine <74672023+Sefdine@users.noreply.github.com>
2023-04-05 18:13:18 +0000

    Change module model

--- src/model/module.php
+++ src/model/module.php
@@ -14,19 +14,21 @@ trait Module
     public function getModules(string $level, string $group_slug, string $study, string $year): array
     {
         $connection = new Database;
-        $statement = $connection->getConnection()->prepare(
-            'SELECT m.name AS module_name, m.slug FROM modules m 
-            JOIN contain_modules cm ON m.id = cm.module_id
-            JOIN contain c ON c.id = cm.contain_id
-            JOIN groupes g ON g.id = c.group_id
-            JOIN levels l ON l.id = c.level_id
-            JOIN studies s ON s.id = c.study_id
-            JOIN years y ON y.id = c.year_id 
+        $statement = $connection->getConnection()->prepare('
+            SELECT m.name AS module_name, m.slug FROM modules m 
+            JOIN levels_modules lm ON m.id = lm.module_id
+            JOIN levels l ON l.id = lm.level_id
+            JOIN groupes_levels gl ON l.id = gl.level_id
+            JOIN groupes g ON g.id = gl.group_id
+            JOIN studies_groupes sg ON g.id = sg.group_id
+            JOIN studies s ON s.id = sg.study_id
+            JOIN years_studies ys ON s.id = ys.study_id
+            JOIN years y ON y.id = ys.year_id
             WHERE l.level = ?
             AND g.slug = ?
             AND s.name = ?
-            AND y.name = ?'
-        );
+            AND y.name = ?
+        ');
         $statement->execute([$level, $group_slug, $study, $year]);
         $modules = [];
         while($row = $statement->fetch()) {
@@ -42,17 +44,16 @@ trait Module
     public function getModulesStudent(int $identifier, string $year): array
     {
         $connection = new Database;
-        $statement = $connection->getConnection()->prepare(
-            'SELECT m.name FROM modules m
-            JOIN contain_modules cm ON m.id = cm.module_id
-            JOIN contain c ON c.id = cm.contain_id
-            JOIN years y ON y.id = c.year_id
-            JOIN registrations r ON c.id = r.contain_id
+        $statement = $connection->getConnection()->prepare('
+            SELECT m.name FROM modules m
+            JOIN levels_modules lm ON m.id = lm.module_id
+            JOIN registrations r ON lm.level_id = r.level_id
+            JOIN years y ON y.id = r.year_id
             JOIN students s ON s.id = r.student_id
             JOIN users u ON u.id = s.user_id
-            AND u.id = ?
-            AND y.name = ?'
-        );
+            WHERE u.id = ?
+            AND y.name = ?
+        ');
         $statement->execute([$identifier, $year]);
         $modules = [];
         while($row = $statement->fetch()) {
