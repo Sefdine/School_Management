224a2c5078f7160f86217f590fdfcb13601881ce
Sefdine <74672023+Sefdine@users.noreply.github.com>
2023-04-11 14:30:24 +0000

    Change module model for new db

--- src/model/module.php
+++ src/model/module.php
@@ -11,25 +11,21 @@ trait Module
     public $name;
     public $slug;
 
-    public function getModules(string $level, string $group_slug, string $study, string $year): array
+    public function getModules(string $group, string $study, string $year): array
     {
         $connection = new Database;
         $statement = $connection->getConnection()->prepare('
             SELECT m.name AS module_name, m.slug FROM modules m 
-            JOIN levels_modules lm ON m.id = lm.module_id
-            JOIN levels l ON l.id = lm.level_id
-            JOIN groupes_levels gl ON l.id = gl.level_id
-            JOIN groupes g ON g.id = gl.group_id
-            JOIN studies_groupes sg ON g.id = sg.group_id
-            JOIN studies s ON s.id = sg.study_id
+            JOIN studies_groupes_modules sgm ON m.id = sgm.module_id
+            JOIN groupes g ON g.id = sgm.group_id
+            JOIN studies s ON s.id = sgm.study_id
             JOIN years_studies ys ON s.id = ys.study_id
             JOIN years y ON y.id = ys.year_id
-            WHERE l.level = ?
-            AND g.slug = ?
+            AND g.group_number = ?
             AND s.name = ?
             AND y.name = ?
         ');
-        $statement->execute([$level, $group_slug, $study, $year]);
+        $statement->execute([$group, $study, $year]);
         $modules = [];
         while($row = $statement->fetch()) {
             $module = new Self;
@@ -46,8 +42,8 @@ trait Module
         $connection = new Database;
         $statement = $connection->getConnection()->prepare('
             SELECT m.name FROM modules m
-            JOIN levels_modules lm ON m.id = lm.module_id
-            JOIN registrations r ON lm.level_id = r.level_id
+            JOIN studies_groupes_modules sgm ON m.id = sgm.module_id
+            JOIN registrations r ON sgm.group_id = r.group_id
             JOIN years y ON y.id = r.year_id
             JOIN students s ON s.id = r.student_id
             JOIN users u ON u.id = s.user_id
