3b7c0cb0905667a4644bb463f80c2461ea6f6f4e
Sefdine <74672023+Sefdine@users.noreply.github.com>
2023-04-11 14:47:25 +0000

    Change exam for new db

--- src/model/exam.php
+++ src/model/exam.php
@@ -12,22 +12,45 @@ trait Exam
     {
         $connection = new Database;
         $statement = $connection->getConnection()->query(
-            'SELECT number FROM exams ORDER BY number ASC'
+            'SELECT exam_name FROM exams ORDER BY id ASC'
         );
         $controls = [];
         while($row = $statement->fetch()) {
-            $controls[] = $row['number'];
+            $controls[] = $row['exam_name'];
         }
         return $controls;
     }
 
-    public function getIdExam(string $number): int 
+    public function getIdExam(string $exam_name): int 
     {
         $connection = new Database;
         $statement = $connection->getConnection()->prepare(
-            'SELECT id FROM exams WHERE number = ?'
+            'SELECT id FROM exams WHERE exam_name = ?'
         );
-        $statement->execute([$number]);
+        $statement->execute([$exam_name]);
         return ($row = $statement->fetch()) ? (int)$row['id'] : 0;
     }
+
+    public function getExamsTypes(): array
+    {
+        $connection = new Database;
+        $statement = $connection->getConnection()->query(
+            'SELECT exam_type FROM exams_types ORDER BY id ASC'
+        );
+        $controls = [];
+        while($row = $statement->fetch()) {
+            $controls[] = $row['exam_type'];
+        }
+        return $controls;
+    }
+
+    public function getIdExamType(string $exam_type): int 
+    {
+        $connection = new Database;
+        $statement = $connection->getConnection()->prepare(
+            'SELECT id FROM exams_types WHERE exam_type = ?'
+        );
+        $statement->execute([$exam_type]);
+        return ($row = $statement->fetch()) ? (int)$row['id'] : 0;
+    }    
 }
